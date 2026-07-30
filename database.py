0104fa91436f628360072a6b9b66fef0820e05d8
marianappmoreira-max <marianappmoreira@gmail.com>
2026-07-29 23:51:31 -0300

    database.py

--- database.py
+++ database.py
@@ -1,19 +1,73 @@
-import json
+import sqlite3
 
 
-ARQUIVO_USUARIOS = "dados/usuarios.json"
+BANCO = "classhub.db"
 
 
-def carregar_usuarios():
-    with open(ARQUIVO_USUARIOS, "r", encoding="utf-8") as arquivo:
-        return json.load(arquivo)
+def conectar():
+    return sqlite3.connect(BANCO)
 
 
-def salvar_usuarios(usuarios):
-    with open(ARQUIVO_USUARIOS, "w", encoding="utf-8") as arquivo:
-        json.dump(
-            usuarios,
-            arquivo,
-            indent=4,
-            ensure_ascii=False
+def criar_tabelas():
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            usuario TEXT UNIQUE NOT NULL,
+            senha TEXT NOT NULL,
+            tipo TEXT NOT NULL
         )
+    """)
+
+    conexao.commit()
+    conexao.close()
+
+
+def adicionar_usuario(nome, usuario, senha, tipo):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        INSERT INTO usuarios
+        (nome, usuario, senha, tipo)
+        VALUES (?, ?, ?, ?)
+    """, (nome, usuario, senha, tipo))
+
+    conexao.commit()
+    conexao.close()
+
+
+def buscar_usuario(usuario):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        SELECT * FROM usuarios
+        WHERE usuario = ?
+    """, (usuario,))
+
+    resultado = cursor.fetchone()
+
+    conexao.close()
+
+    return resultado
+
+
+def listar_alunos():
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        SELECT nome, usuario
+        FROM usuarios
+        WHERE tipo = 'aluno'
+    """)
+
+    resultado = cursor.fetchall()
+
+    conexao.close()
+
+    return resultado
